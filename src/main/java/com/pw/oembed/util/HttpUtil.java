39fea3604ea351a67dd4d524e041e769a1dec508
dongkos <dhkim931101@naver.com>
2022-04-30 00:01:35 +0900

    Refactor: Objectmapper DI

--- src/main/java/com/pw/oembed/util/HttpUtil.java
+++ src/main/java/com/pw/oembed/util/HttpUtil.java
@@ -4,22 +4,23 @@ import org.springframework.stereotype.Component;
 
 import com.pw.oembed.dto.SearchResult;
 
+import lombok.RequiredArgsConstructor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 @Component
+@RequiredArgsConstructor
 public class HttpUtil {
 
+	private final MyObjectMapper mapper;
+	
 	public SearchResult get(String url) throws Exception {
 		OkHttpClient client = new OkHttpClient();
 		Request request = new Request.Builder()
 				.url(url)
 				.build();
-		
 		Response response = client.newCall(request).execute();
-		String result = response.body().string();
-		
-		return new MyObjectMapper().readValue(result, SearchResult.class);
+		return mapper.readValue(response.body().string(), SearchResult.class);
 	}
 }
