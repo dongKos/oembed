b6de772403035994f2f46e873beab16df9c8015e
dongkos <dhkim931101@naver.com>
2022-04-29 23:58:56 +0900

    Style: unused import 제거

--- src/main/java/com/pw/oembed/controller/OembedController.java
+++ src/main/java/com/pw/oembed/controller/OembedController.java
@@ -1,10 +1,8 @@
 package com.pw.oembed.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pw.oembed.dto.SearchRequest;
 import com.pw.oembed.dto.SearchResult;
 import com.pw.oembed.service.OembedService;
 
