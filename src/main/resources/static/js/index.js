0202859965eb8960140be1aa94cfccd67fff62b3
dongkos <dhkim931101@naver.com>
2022-04-30 15:13:57 +0900

    Style: 프론트 스타일 변경 css 적용 및 ajax before, complete에 opacity 속성 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,31 +1,49 @@
 $(function() {
+	$(document).on('keyup', handleKeyup);
 	$('#search').on('click', getOembedData);
 });
+
+function handleKeyup(e) {
+	const KEY_ENTER = 13;
+	if(e.keyCode == KEY_ENTER && $('#url').val().length > 0) {
+		return getOembedData();
+	}
+	
+	if(e.keyCode == KEY_ENTER && $('#url').val().length == 0) {
+		return alert('검색어를 입력해주세요!');
+	}
+}
 
 function getOembedData() {
 	$.ajax({
 		url: '/search?url='+$('#url').val(),
 		contentType: 'application/json',
+		beforeSend: function() {
+			$('body').css('opacity', 0.5);
+		},
 		success: function(data) {
 			var dataObj = data;
-			$('#oembedTable tbody').html('');
+			var html = "";
+			$('#oembedTable tbody').html(html);
 			for(key in dataObj) {
 				if(dataObj[key] != null) {
 					html += "<tr>";
 					html += 	"<td>" + key + "</td>";
-					html += 	"<td>" + dataObj[key] + "</td>";
+					html += 	"<td name='" + key + "'>" + dataObj[key] + "</td>";
 					html += "</tr>";
 				}
 			}
 			$('#oembedTable tbody').append(html);
 		},
 		error: function(err) {
-			console.log(err);
 			if(err.responseText != null) {
 				$('#oembedTable tbody').html('');
 				var html = "<tr align='center'><td>" + err.responseText + "</td></tr>";
 				$('#oembedTable tbody').append(html);
 			}
+		},
+		complete: function() {
+			$('body').css('opacity', 1);
 		}
 	})
 }
